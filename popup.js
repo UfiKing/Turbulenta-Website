2cc66a6a4ba8847ee1e31dd774f9a287281b37c4
UfiKing <jure@hodak.net>
2024-06-25 17:45:16 +0200

    Added artifacts from season 2 to items page

--- popup.js
+++ popup.js
@@ -9,6 +9,14 @@
         var modal7 = document.getElementById("popLS");
         var modal8 = document.getElementById("popHOS");
         var modal9 = document.getElementById("popROK");
+        var modal10 = document.getElementById("popTD");
+        var modal11 = document.getElementById("popH");
+        var modal12 = document.getElementById("popDC");
+        var modal13 = document.getElementById("popVOM");
+        var modal14 = document.getElementById("popDTOAM");
+        var modal15 = document.getElementById("popCOB");
+        var modal16 = document.getElementById("popSOS");
+        var modal17 = document.getElementById("popCC");
 
         // Get the button that opens the modal
         var btn = document.getElementById("btnROSD");
@@ -21,7 +29,14 @@
         var btn7 = document.getElementById("btnLS");
         var btn8 = document.getElementById("btnHOS")
         var btn9 = document.getElementById("btnROK")
-
+        var btn10 = document.getElementById("btnTD");
+        var btn11 = document.getElementById("btnH");
+        var btn12 = document.getElementById("btnDC");
+        var btn13 = document.getElementById("btnVOM");
+        var btn14 = document.getElementById("btnDTOAM");
+        var btn15 = document.getElementById("btnCOB");
+        var btn16 = document.getElementById("btnSOS");
+        var btn17 = document.getElementById("btnCC");
 
         // Get the <span> element that closes the modal
         //var span = document.getElementsByClassName("close")[0];
@@ -35,6 +50,14 @@
         var span7 = document.getElementById("close7")
         var span8 = document.getElementById("close8")
         var span9 = document.getElementById("close9")
+        var span10 = document.getElementById("close")
+        var span11 = document.getElementById("close10")
+        var span12 = document.getElementById("close12")
+        var span13 = document.getElementById("close13")
+        var span14 = document.getElementById("close14")
+        var span15 = document.getElementById("close15")
+        var span16 = document.getElementById("close16")
+        var span17 = document.getElementById("close17")
 
     // When the user clicks on the button, open the modal
     btn.onclick = function() {
@@ -67,6 +90,30 @@
     btn9.onclick = function() {
         modal9.style.display = "block";
     }
+    btn10.onclick = function() {
+        modal10.style.display = "block";
+    }
+    btn11.onclick = function() {
+        modal11.style.display = "block";
+    }
+    btn12.onclick = function() {
+        modal12.style.display = "block";
+    }
+    btn13.onclick = function() {
+        modal13.style.display = "block";
+    }
+    btn14.onclick = function() {
+        modal14.style.display = "block";
+    }
+    btn15.onclick = function() {
+        modal15.style.display = "block";
+    }   
+    btn16.onclick = function() {
+        modal16.style.display = "block";
+    }
+    btn17.onclick = function() {
+        modal17.style.display = "block";
+    }
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
@@ -98,6 +145,30 @@
     span9.onclick = function() {
         modal9.style.display = "none";
     }
+    span10.onclick = function() {
+        modal10.style.display = "none";
+    }
+    span11.onclick = function() {
+        modal11.style.display = "none";
+    }
+    span12.onclick = function() {
+        modal12.style.display = "none";
+    }
+    span13.onclick = function() {
+        modal13.style.display = "none";
+    }
+    span14.onclick = function() {
+        modal14.style.display = "none";
+    }
+    span15.onclick = function() {
+        modal15.style.display = "none";
+    }
+    span16.onclick = function() {
+        modal16.style.display = "none";
+    }
+    span17.onclick = function() {
+        modal17.style.display = "none";
+    }
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
@@ -120,7 +191,24 @@
             modal8.style.display = "none";
         }else if(event.target == modal9){
             modal9.style.display = "none";
+        }else if(event.target == modal10){
+            modal10.style.display = "none";
+        }else if(event.target == modal11){
+            modal11.style.display = "none";
+        }else if(event.target == modal12){
+            modal12.style.display = "none";
+        }else if(event.target == modal13){
+            modal13.style.display = "none";
+        }else if(event.target == modal14){
+            modal14.style.display = "none";
+        }else if(event.target == modal15){
+            modal15.style.display = "none";
+        }else if(event.target == modal16){
+            modal16.style.display = "none";
+        }else if(event.target == modal17){
+            modal17.style.display = "none";
         }
+        
     }
     /*window.onclick = function(event) {
         if (event.target == modal1) {
